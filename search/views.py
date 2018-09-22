b8f444b5611c9438da121bb2719a03f3b4792d89
PrajwalRavi <prjwl132@gmail.com>
2018-09-22 15:48:04 +0530

    Fixed bugs. UI updated for spelling check.

--- search/views.py
+++ search/views.py
@@ -46,6 +46,9 @@ def get_result(message):
             pos = get_wordnet_pos(word[1])  # POS conversion to map with WordNet Lemmatizer
             lemmatized_words.append(lemmatizer.lemmatize(word[0], pos))
 
+    if len(lemmatized_words) == 0:
+        return [-1]
+
     tf_idf_file = open('search/tf-idf.dat', 'rb')  # Unpickling the stored tf-idf file for query matching and ranking
     dict_tf_idf = pickle.load(tf_idf_file)
     tf_idf_file.close()
@@ -70,52 +73,42 @@ def get_result(message):
     return answer
 
 
-def phraseQuery(phrase_query):
+def phrase_query_search(phrase_query):
+    print(phrase_query)
     phraseResult = []
-    for file_no in range(0, 50):  # Run through each file to collect the words
+    for file_no in range(0, 309):  # Run through each file to collect the words
         with open("Corpus/" + str(file_no) + ".txt", "r") as file:
             tokens = nltk.word_tokenize(file.read())
 
         for key in range(0, len(tokens)):
-            count = 0
-            for i in range(0, len(phrase_query)):
-                if i + key < len(tokens) and phrase_query[i] == tokens[i + key]:
-                    count += 1
-                else:
-                    break
+            if tokens[key] == phrase_query[0]:
+                count = 0
+                for i in range(0, len(phrase_query)):
+                    if i + key < len(tokens) and phrase_query[i] == tokens[i + key]:
+                        count += 1
 
-            if count == len(phrase_query):
-                phraseResult.append(file_no)
-                break
+                    if count == len(phrase_query):
+                        phraseResult.append(file_no)
 
+                        break
     return phraseResult
 
 
-def editDistDP(str1, str2, m, n):
-    # Create a table to store results of subproblems
-    dp = [[0 for x in range(n + 1)] for x in range(m + 1)]
+def edit_dist(str1, str2, m, n):
+    dp = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
 
-    # Fill d[][] in bottom up manner
     for i in range(m + 1):
         for j in range(n + 1):
 
-            # If first string is empty, only option is to
-            # isnert all characters of second string
             if i == 0:
                 dp[i][j] = j  # Min. operations = j
 
-            # If second string is empty, only option is to
-            # remove all characters of second string
             elif j == 0:
                 dp[i][j] = i  # Min. operations = i
 
-            # If last characters are same, ignore last char
-            # and recur for remaining string
             elif str1[i - 1] == str2[j - 1]:
                 dp[i][j] = dp[i - 1][j - 1]
 
-                # If last character are different, consider all
-            # possibilities and find minimum
             else:
                 dp[i][j] = 1 + min(dp[i][j - 1],  # Insert
                                    dp[i - 1][j],  # Remove
@@ -131,46 +124,55 @@ def search(request):
         search results.
     """
     start_time = time.time()
-    message = request.GET.get('query')
-    if message[0] == "'" and message[len(message) - 1] == "'":
-        phrase = nltk.word_tokenize(message)
+    search_query = request.GET.get('query')
+    did_you_mean = ""
+
+    if search_query and search_query[0] == "'" and search_query[len(search_query) - 1] == "'":
+        phrase = nltk.word_tokenize(search_query)
         phrase.remove("'")
         phrase.remove("'")
-        file_results = phraseQuery(phrase)
+        file_results = phrase_query_search(phrase)
+        print(file_results)
 
     else:
-        file_results = get_result(message)
-        if len(file_results) == 0:
+        file_results = get_result(search_query)
+
+        if file_results and file_results[0] == -1:
+            pass
+
+        elif len(file_results) == 0:
             word_file = open('search/words.dat', 'rb')
             words = pickle.load(word_file)
             word_file.close()
-            message = nltk.word_tokenize(message)
+            search_query = nltk.word_tokenize(search_query)
 
-            for i in range(0, len(message)):
+            for i in range(0, len(search_query)):
                 min_edit = 9999999
-                new_word = message[i]
+                new_word = search_query[i]
                 for each_word in words:
-                    edit_dis = editDistDP(message[i], each_word,
-                                          len(message[i]),
-                                          len(each_word))
+                    edit_dis = edit_dist(search_query[i], each_word,
+                                         len(search_query[i]),
+                                         len(each_word))
                     # print(edit_dis)
-                    if edit_dis<min_edit:
+                    if edit_dis < min_edit:
                         new_word = each_word
                         min_edit = edit_dis
 
-                message[i]=new_word
-                print(new_word  )
+                search_query[i] = new_word
+                print(new_word)
 
-            message = " ".join(message)
-            file_results = get_result(message)
+            search_query = " ".join(search_query)
+            did_you_mean = search_query
+            file_results = get_result(search_query)
 
     file_objects = []  # stores the file object associated with each file number in file_results
     poem_names = []
     for file_num in file_results:
-        obj = Poem.objects.get(id=file_num)
-        if obj.title not in poem_names:
-            file_objects.append(obj)
-            poem_names.append(obj.title)
+        if file_num >= 0:
+            obj = Poem.objects.get(id=file_num)
+            if obj.title not in poem_names:
+                file_objects.append(obj)
+                poem_names.append(obj.title)
 
     number_of_results = len(file_objects)
     if number_of_results > 10:
@@ -179,8 +181,8 @@ def search(request):
     end_time = time.time()
     time_taken = end_time - start_time
     return render(request, 'search/main_page.html',
-                  {'results': file_objects, 'org_query': message, 'time_taken': time_taken,
-                   'number_of_results': number_of_results})
+                  {'results': file_objects, 'org_query': search_query, 'time_taken': time_taken,
+                   'number_of_results': number_of_results, 'did_you_mean': did_you_mean})
 
 
 def display_file(request, file_id):
